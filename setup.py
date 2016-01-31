2ab2a3ca4bb643c82e47fbc2e5a49e61084518d1
Tim <tim@weeble.info>
2016-01-31 13:28:44 +0000

    Increase version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='liffylights',
     py_modules=['liffylights'],
-    version='0.9.2',
+    version='0.9.3',
     description='LIFX Python library',
     author='TangoAlpha',
     author_email='liffylights@attwood.org',
